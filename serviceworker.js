cb3429415fa160519822a1b5df7821f34475181c
AkashG6 <akashgarule@gmail.com>
2018-06-23 14:50:13 +0530

    add offline page

--- serviceworker.js
+++ serviceworker.js
@@ -1,11 +1,12 @@
 console.log('sw');
 
-const STATIC_CACHE = "1.0"; // change version no. when changes are made so that browser will pick new service worker
+const STATIC_CACHE = "1.2"; // change version no. when changes are made so that browser will pick new service worker
 // const DYNAMIC_CACHE = "1.0";
 
 var urlsToCache = [
 	'/',
 	'/index.html',
+	'/about.html',
 	'/dist/main.css',
 	'/dist/bundle.js',
 	'/resources/scripts/script.js',
@@ -16,6 +17,20 @@ self.addEventListener('install',(event) => {
 		caches.open(STATIC_CACHE).then((cache)=>{
 			console.log('cache opened');
 			return cache.addAll(urlsToCache);
+		})
+	)
+})
+
+//getting the pages up offine
+
+self.addEventListener('fetch',(event)=>{
+	event.respondWith(
+		caches.match(event.request)
+		.then((response)=>{
+			if (response) {
+				return response;
+			}
+			return fetch(event.request);
 		})
 	)
 })
